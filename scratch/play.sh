8a2f9f0ed90743e8c8cb61be202fcc064255afc3
Bartosz Wróblewski <bawr@hszm.pl>
2020-11-22 19:59:24 -0800

    Absolutely least intrusive lua env interaction.

--- scratch/play.sh
+++ scratch/play.sh
@@ -3,17 +3,11 @@
 set -e
 set -u
 
-LRS="${HOME}/.luarocks/share/lua/5.1"
-LRL="${HOME}/.luarocks/lib/lua/5.1"
-
 export LUA_PATH=$(tr '\n' ';' <<__
 ../lua/?.lua
 ../lua/?/init.lua
-$(luarocks path --lr-path)
+${LUA_PATH:-}
 __
 )
 
-export LUA_CPATH=$(luarocks path --lr-cpath)
-
-# exec lua5.1 ./play.lua "$@"
-exec luajit ./play.lua "$@"
+exec lua ./play.lua "$@"
