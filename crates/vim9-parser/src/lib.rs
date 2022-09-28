5b8ae821225bcf03d5870fd375c10a6a8cefde41
TJ DeVries <devries.timothyj@gmail.com>
2022-09-28 13:45:14 -0400

    parse elseif and else

--- crates/vim9-parser/src/lib.rs
+++ crates/vim9-parser/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fmt::Debug;
 
@@ -448,6 +449,18 @@ pub struct Body {
 }
 
 impl Body {
+    pub fn parse_until_any(
+        parser: &mut Parser,
+        identifiers: &HashSet<String>,
+    ) -> Result<Body> {
+        let mut commands = vec![];
+        while !identifiers.contains(&parser.current_token.text) {
+            commands.push(parser.parse_command()?);
+        }
+
+        Ok(Body { commands })
+    }
+
     pub fn parse_until(parser: &mut Parser, identifier: &str) -> Result<Body> {
         let mut commands = vec![];
         while parser.current_token.text != identifier {
@@ -464,17 +477,70 @@ pub struct IfCommand {
     pub condition: Expression,
     if_eol: Token,
     pub body: Body,
+    pub elseifs: Vec<ElseIfCommand>,
+    pub else_command: Option<ElseCommand>,
     endif_tok: Token,
     endif_eol: Token,
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct ElseIfCommand {
+    elseif_tok: Token,
+    pub condition: Expression,
+    elseif_eol: Token,
+    pub body: Body,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ElseCommand {
+    else_tok: Token,
+    else_eol: Token,
+    pub body: Body,
+}
+
 impl IfCommand {
     pub fn parse(parser: &mut Parser) -> Result<ExCommand> {
+        let if_endings: HashSet<String> = HashSet::from_iter(
+            vec![
+                "elseif".to_string(),
+                "else".to_string(),
+                "endif".to_string(),
+            ]
+            .into_iter(),
+        );
+
         Ok(ExCommand::If(IfCommand {
             if_tok: parser.expect_identifier_with_text("if")?,
             condition: Expression::parse(parser, Precedence::Lowest)?,
             if_eol: parser.expect_eol()?,
-            body: Body::parse_until(parser, "endif")?,
+            body: Body::parse_until_any(parser, &if_endings)?,
+            elseifs: {
+                let mut elseifs = Vec::new();
+                while parser.current_token.text == "elseif" {
+                    elseifs.push(ElseIfCommand {
+                        elseif_tok: parser.pop(),
+                        condition: Expression::parse(
+                            parser,
+                            Precedence::Lowest,
+                        )?,
+                        elseif_eol: parser.expect_eol()?,
+                        body: Body::parse_until_any(parser, &if_endings)?,
+                    })
+                }
+
+                elseifs
+            },
+            else_command: {
+                if parser.current_token.text == "else" {
+                    Some(ElseCommand {
+                        else_tok: parser.pop(),
+                        else_eol: parser.expect_eol()?,
+                        body: Body::parse_until(parser, "endif")?,
+                    })
+                } else {
+                    None
+                }
+            },
             endif_tok: parser.expect_identifier_with_text("endif")?,
             endif_eol: parser.expect_eol()?,
         }))
--- crates/vim9-parser/testdata/snapshots/if.vim
+++ crates/vim9-parser/testdata/snapshots/if.vim
@@ -4,3 +4,14 @@ vim9script
 if pumvisible() || (str2nr(&t_Co) < 8 && !has('gui_running'))
   finish
 endif
+
+var x = 20
+if x < 10
+  echo "10"
+elseif x < 15
+  echo "15"
+elseif x < 30
+  echo "30"
+else
+  echo "else"
+endif
