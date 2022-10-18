6ba9392450654b58c769926a2799626c15960efb
TJ DeVries <devries.timothyj@gmail.com>
2022-10-18 11:24:34 -0400

    add benchmark
    Before Changes:
    
    test lexer_handler ... bench:  13,528,453 ns/iter (+/- 26,033)
    
    After Changes:
    
    test lexer_handler ... bench:     863,296 ns/iter (+/- 63,561)

---
+++ crates/vim9-lexer/benches/bench.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+
+extern crate test;
+
+#[bench]
+fn lexer_handler(b: &mut test::Bencher) {
+    b.iter(|| {
+        let contents = include_str!(
+            "/home/tjdevries/git/lsp/src/autoload/lsp/handlers.vim"
+        );
+        let lexer = vim9_lexer::Lexer::new(&contents);
+
+        let mut res = vec![];
+        loop {
+            match test::black_box(lexer.next_token()) {
+                Ok(tok) => {
+                    if tok.kind == vim9_lexer::TokenKind::EndOfFile {
+                        break;
+                    }
+
+                    res.push(tok);
+                }
+                Err(err) => assert!(false, "error: {:?}", err),
+            }
+        }
+
+        assert!(res.len() > 0, "Must have some items");
+    });
+}
