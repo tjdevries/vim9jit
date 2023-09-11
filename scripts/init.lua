a902801ddc89e10728def73e8d33e0fbfdfe09d5
TJ DeVries <devries.timothyj@gmail.com>
2023-09-11 12:47:55 -0400

    ci: use vendored plenary

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,12 +1,6 @@
--- Add target dir, if not exists
-vim.fn.mkdir('target', 'p')
-
-if not vim.loop.fs_stat('target/plenary.nvim') then
-  vim
-    .system({ 'git', 'clone', 'https://github.com/nvim-lua/plenary.nvim', 'target/plenary.nvim' })
-    :wait()
-end
-
-vim.opt.rtp:append({ '.', './target/plenary.nvim' })
+vim.opt.rtp:append({
+  '.',
+  vim.fn.expand('~/.local/share/nvim/site/pack/vendor/start/plenary.nvim'),
+})
 
 vim.cmd([[runtime! plugin/plenary.vim]])
